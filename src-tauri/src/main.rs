6ee916da9e07ec5a8f23c07386a9c0224b9c6df9
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2023-12-04 23:26:46 +0000

    :sparkles: feat: first commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,47 @@
+/*
+ * @format
+ * -----
+ * Project: my-qwik-basic-starter
+ * File: main.rs
+ * Path: \src-tauri\src\main.rs
+ * Created Date: Thursday, November 23rd 2023
+ * Author: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
+ * -----
+ * Contributing: Please read through our contributing guidelines. Included are directions for opening
+ * issues, coding standards, and notes on development. These can be found at https://github.com/my-qwik-basic-starter/blob/develop/CONTRIBUTING.md
+ * 
+ * Code of Conduct: This project abides by the Contributor Covenant, version 2.0. Please interact in ways that contribute to an open,
+ * welcoming, diverse, inclusive, and healthy community. Our Code of Conduct can be found at https://github.com/my-qwik-basic-starter/blob/develop/CODE_OF_CONDUCT.md
+ * -----
+ * Copyright (c) 2023 Eventiva - All Rights Reserved
+ * LICENSE: Creative Commons Zero v1.0 Universal (CC0-1.0)
+ * -----
+ * This program has been provided under confidence of the copyright holder and is 
+ * licensed for copying, distribution and modification under the terms of
+ * the Creative Commons Zero v1.0 Universal (CC0-1.0) published as the License,
+ * or (at your option) any later version of this license.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Creative Commons Zero v1.0 Universal for more details.
+ * 
+ * You should have received a copy of the Creative Commons Zero v1.0 Universal
+ * along with this program. If not, please write to: jonathan.stevens@eventiva.co.uk,
+ * or see https://creativecommons.org/publicdomain/zero/1.0/legalcode
+ * 
+ * DELETING THIS NOTICE AUTOMATICALLY VOIDS YOUR LICENSE - PLEASE SEE THE LICENSE FILE FOR DETAILS
+ * -----
+ * Last Modified: 23-11-2023
+ * By: Jonathan Stevens (Email: jonathan.stevens@eventiva.co.uk, Github: https://github.com/TGTGamer)
+ * Current Version: <<projectversion>>
+ */
+
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
